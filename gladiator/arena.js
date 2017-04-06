6e93c297e08c2ff00451b48261c5c491eb770b84
J Silverstein <jess.pebble@gmail.com>
2017-04-05 20:39:37 -0400

    initial commit, setting up file structure

---
+++ gladiator/arena.js
@@ -0,0 +1,4 @@
+console.log('arena.js is connected!');
+
+
+// Write your Arena class in this file!!!
---
+++ gladiator/gladiator.js
@@ -0,0 +1,4 @@
+console.log('gladiator.js is connected!');
+
+
+// Write your Gladiator class in this file!!!!
