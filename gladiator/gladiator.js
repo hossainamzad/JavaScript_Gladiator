7deb93bc170df4b8ba4a4035d36e633aecc0d4ff
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2017-07-26 18:20:21 -0400

    built gladiator and arena objects and made the game functional.

--- gladiator/gladiator.js
+++ gladiator/gladiator.js
@@ -2,3 +2,24 @@ console.log('gladiator.js is connected!');
 
 
 // Write your Gladiator class in this file!!!!
+class Gladiator {
+  constructor (name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+}
+const max = new Gladiator("Maximus", "Trident");
+console.log(max.name);
+console.log(max.weapon);
+
+const titus = new Gladiator("Titus", "Club");
+console.log(titus.name + " " + titus.weapon);
+const andro = new Gladiator("Andronicus", "Sword");
+console.log(andro.name);
+console.log(andro.weapon);
+
+const heral = new Gladiator("Herald", "Archer");
+console.log(heral.name);
+console.log(heral.weapon);
+
+
